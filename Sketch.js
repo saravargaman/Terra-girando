ec7f4264c0f0f855bde43380328e67ce15390362
saravargaman <matheus.reis.andre@escola.pr.gov.br>
2025-08-05 09:37:46 -0300

    Update Sketch.js

--- Sketch.js
+++ Sketch.js
@@ -1,14 +1,11 @@
-
 function setup() {
-  createCanvas(1920, 1080, WEBGL); //plano 3D
-  terra=loadImage('terra.jpg')
+  createCanvas(1920, 1080, WEBGL); // plano 3D
+  terra = loadImage('terra.jpg');
 }
 
 function draw() {
-  background(205, 102, 94); //cor de fundo
-  rotateY(millis()/1000); // rotação da terra
-  texture(terra); //textura da terra, imagem.
-  sphere(80, 100); //esfera 3D
-  
-  
+  background(128, 0, 128); // Cor de fundo roxa (RGB: 128, 0, 128)
+  rotateY(millis() / 1000); // rotação da terra
+  texture(terra); // textura da terra, imagem.
+  sphere(80, 100); // esfera 3D
 }
